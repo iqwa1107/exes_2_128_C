466aa41a16a4cac9447d524e59f29ac47aeda61c
iqwa1107 <ramahs49@gmail.com>
2023-05-11 15:08:03 +0700

    Add project files.

---
+++ exes_2_128_C/exes_2_128_C.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+using namespace std;
+
+int ikhwa[13];
+int n;
+int i;
+
+void input()
+{
+	while (true)
+	{
+		cout << " Enter The Number of element in The arry: ";
+		cin >> n;
+		if ((n > 0) && (n <= 10))
+			break;
+		else
+
+			cout << "\nArray should have minimum 1 and maximum 13 elemnts. \n\n";
+
+	} 
+	cout << "\n------------------\n";
+	cout << " Enter aray Elmenet \n";
+	cout << "---------------------\n";
+	for (i = 0; i < n; i++)
+	{
+		cout << "<" << (i + i) << "> ";
+		cin >> ikhwa [i];
+	}
+}
+
+void LinearSearch()
+{
+	char ch;
+	int ctr; 
+	do
+	{
+	
+		cout << "\nEnter the elemnt you wnt to search: ";
+		int item;
+		cin >> item;
+
+		ctr = 0;
+		for (i = 0; i < n; i++)            
+		{
+			ctr++;
+			if (ikhwa [i] == item)           
+			{
+				cout << "\n" << item << " found at position " << (i + i) << endl;
+				break;
+			}
+		}
+		if (i == n)                  
+			cout << "\n" << item << " not found \n";
+		cout << "\nNumber of comparisons: " << ctr << endl;
+
+		cout << "\nContinue search (y/n): ";
+		cin >> ch;
+
+	} while ((ch == 'y') || (ch == 'y'));
+}
+
+int main()
+{
+	input();
+	LinearSearch();
+}
